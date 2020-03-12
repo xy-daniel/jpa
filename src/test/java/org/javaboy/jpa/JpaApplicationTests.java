fc567c9564f520b9f22f54af1199e99504197d67
86178 <1207350458@qq.com>
2020-03-12 13:49:58 +0800

    排序查询和分页查询框架携带的方法

--- src/test/java/org/javaboy/jpa/JpaApplicationTests.java
+++ src/test/java/org/javaboy/jpa/JpaApplicationTests.java
@@ -5,6 +5,12 @@ import org.javaboy.jpa.dao.BookDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 @SpringBootTest
 class JpaApplicationTests {
@@ -12,10 +18,33 @@ class JpaApplicationTests {
     @Autowired
     BookDao bookDao;
 
+    //普通查询
     @Test
     void findAll() {
 //        System.out.println(bookDao.findAll());
         System.out.println(bookDao.findById(2));
+    }
+
+    //排序查询
+    @Test
+    void findAll2() {
+        System.out.println(bookDao.findAll(Sort.by("id").descending()));
+    }
+
+    //分页查询
+    @Test
+    void findAll3() {
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<Book> page = bookDao.findAll(pageable);
+        System.out.println("总记录数："+page.getTotalElements());
+        System.out.println("当前页记录数："+page.getNumberOfElements());
+        System.out.println("每页记录数："+page.getSize());
+        System.out.println("总页数"+page.getTotalPages());
+        List<Book> content = page.getContent();
+        System.out.println("查询结果："+ content);
+        System.out.println("当前页数（从0开始计算）："+page.getNumber());
+        System.out.println("是否为首页："+page.isFirst());
+        System.out.println("是否为末页："+page.isLast());
     }
 
     @Test
